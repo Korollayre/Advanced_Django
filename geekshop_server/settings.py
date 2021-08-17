ea3d8351ec8b5ac2952fc92af94e7ae7bf1a3f2b
Korollayre <mbuinichienko@mail.ru>
2021-08-17 11:16:51 +0300

    Added ajax script for orders

--- geekshop_server/settings.py
+++ geekshop_server/settings.py
@@ -85,8 +85,9 @@ WSGI_APPLICATION = 'geekshop_server.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'geekshop_db',
+        'USER': 'postgres',
     }
 }
 
