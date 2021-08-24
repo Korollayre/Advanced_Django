c327ff239405302dd870d8e3d45639f589a6edc5
Korollayre <mbuinichienko@mail.ru>
2021-08-24 14:03:55 +0300

    Change STATIC_ROOT

--- geekshop_server/settings.py
+++ geekshop_server/settings.py
@@ -160,7 +160,7 @@ STATIC_URL = '/static/'
 #     BASE_DIR / 'static',
 # )
 
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
