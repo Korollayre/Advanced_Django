0a423da8de9ab7580df1e6b0b2d7bfe279165688
Korollayre <mbuinichienko@mail.ru>
2021-08-31 11:50:50 +0300

    Added memcached settings to settings.py

--- geekshop_server/settings.py
+++ geekshop_server/settings.py
@@ -118,9 +118,9 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'geekshop_db',
         'USER': 'postgres',
-        'PASSWORD': 231096,
-        'HOST': '127.0.0.1',
-        'PORT': '5432'
+        # 'PASSWORD': 231096,
+        # 'HOST': '127.0.0.1',
+        # 'PORT': '5432'
     }
 }
 
@@ -215,3 +215,17 @@ SOCIAL_AUTH_PIPELINE = (
     'social_core.pipeline.social_auth.load_extra_data',
     'social_core.pipeline.user.user_details',
 )
+
+if os.name == 'posix':
+    CACHE_MIDDLEWARE_ALIAS = 'default'
+    CACHE_MIDDLEWARE_SECONDS = 120
+    CACHE_MIDDLEWARE_KEY_PREFIX = 'geekshop'
+
+    CACHES = {
+        'default': {
+            'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+            'LOCATION': '127.0.0.1:11211',
+        }
+    }
+
+LOW_CACHE = True
