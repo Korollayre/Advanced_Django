9e5ac721c9c25957c7513a9cb5347d26192850e7
Korollayre <mbuinichienko@mail.ru>
2021-09-02 12:52:34 +0300

    Add cache_page for "products" controller in products/urls.py

--- products/urls.py
+++ products/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from products.views import products
+from django.views.decorators.cache import cache_page
 
 app_name = 'products'
 
 urlpatterns = [
     path('', products, name='index'),
-    path('<int:category_id>/', products, name='product'),
+    path('<int:category_id>/', cache_page(3600)(products), name='product'),
     path('page/<int:page>/', products, name='page'),
 ]
